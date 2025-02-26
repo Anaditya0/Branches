5b362c907840f0d9de87c9e2fcab73b6439b821d
Anaditya0 <andysaini1303@gmail.com>
2025-02-26 17:08:43 +0530

    After resolving merge conflict

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 Testing stashing
-Console.log(Stash1)
+console.log(Stash 1)
+console.log(Stash 2)
+
+If changes were made in same line in previous stash, it will show us merge conflict so we have to be very careful while stashing our files.
